00492b79eeef0df2807ef2e05e8220567ec8897a
andkirsten <and.kirsten@gmail.com>
2024-02-26 21:31:06 +0900

    initial commit

---
+++ kam-portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import Heading from "./sections/heading";
+
+function App() {
+  return (
+    <>
+      <Heading />
+    </>
+  );
+}
+
+export default App;
---
+++ kam-portfolio/src/sections/heading.jsx
@@ -0,0 +1,21 @@
+const Heading = () => (
+  <section className="bg-gray-100">
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+        <div className="order-2 lg:order-1">
+          <h1 className="text-4xl lg:text-6xl font-bold mb-4">
+            Kirsten Andersen Morris
+          </h1>
+          <p className="text-lg mb-4">
+            a software engineer based in India. I specialize in building
+            exceptional digital experiences.
+          </p>
+          <div className="flex"></div>
+        </div>
+        <div className="order-1 lg:order-2"></div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Heading;
